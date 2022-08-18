38c4c75abbe73aece04ccd3983f64f66fd733246
Stas Makarov <stas.makarov@flexiana.com>
2022-08-18 17:11:25 +0400

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to leedn/leedn
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/leedn/leedn_test.clj
@@ -0,0 +1,7 @@
+(ns leedn.leedn-test
+  (:require [clojure.test :refer :all]
+            [leedn.leedn :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
